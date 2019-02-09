c8169e51ca34d6927b1f30f040a446d00a6de526
AbdelRahman Badr <13076756+BilboNuggets@users.noreply.github.com>
2019-02-09 15:47:15 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,5 @@
 from SparkHandler import SparkHandler
 from CassandraHandler import CassandraHandler
 
-# port number 
-PORT_NUMBER = 8080
- 
 cassandraHandler = CassandraHandler("test")
 sparkHandler = SparkHandler(keyspace="test", appName="test")
